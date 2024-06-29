6f154192a3fa3d97a524c876379053c12c499a6e
PhDBui <congdoan6997@gmail.com>
2024-06-29 21:35:34 +0300

    fix: 🐛 toggle theme component

--- components/theme/toggle.tsx
+++ components/theme/toggle.tsx
@@ -1,17 +1,11 @@
 "use client"
 
 import { useTheme } from "next-themes"
-import { useEffect, useState, type HTMLProps } from "react"
-import { Around } from "@theme-toggles/react"
+import { useEffect, useState } from "react"
+import { Around, type ToggleProps } from "@theme-toggles/react"
 import { THEME_DARK, THEME_LIGHT } from "@/components/theme/constants"
 
-import "@theme-toggles/react/css/Around.css"
-
-type ThemeToggleProps = {
-  className?: HTMLProps<HTMLElement>["className"]
-}
-
-export function ThemeToggle({ className }: ThemeToggleProps) {
+export function ThemeToggle(props: ToggleProps) {
   const [mounted, setMounted] = useState(false)
   const { resolvedTheme, setTheme } = useTheme()
 
@@ -24,15 +18,14 @@ export function ThemeToggle({ className }: ThemeToggleProps) {
 
   return (
     <Around
-      duration={500}
       placeholder={isDark ? "🌙" : "☀️"}
       toggled={isDark}
       toggle={() => setTheme(isDark ? THEME_LIGHT : THEME_DARK)}
-      className={className}
       forceMotion
       // TODO - fixes typescript error
       onPointerEnterCapture={undefined}
       onPointerLeaveCapture={undefined}
+      {...props}
     />
   )
 }
